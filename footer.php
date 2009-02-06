95b2b949e9192be10fae91367ce30989ee168a9a
Adam Mckaig <adam.mckaig@gmail.com>
2009-02-06 13:12:26 -0500

    switched to php for includes

---
+++ footer.php
@@ -0,0 +1,9 @@
+			<div id="footer">
+				<a href="/contact/">Contact</a>        &bull;
+				<a href="/tor/">Terms of Reference</a> &bull;
+				<a href="/copyright/">Copyright</a>    &bull;
+				<a href="/press/">Press</a>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		<title></title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div id="masthead">
+					<h1><a href="/">UNICEF Innovations</a></h1>
+					<!--<div id="tagline">We build <strong>Awesome Shit</strong> for children</div>-->
+				</div>
+				<ul id="nav-tabs">
+					<li class="active"><a href="/">Home</a></li>
+					<li><a href="/products/">Products</a></li>
+					<li><a href="/about/">About</a></li>
+				</ul>
+			</div>
+
