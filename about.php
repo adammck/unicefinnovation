7c0c38fef4d35ff732740b128a5850c6e3b5cd36
Adam Mckaig <adam.mckaig@gmail.com>
2009-02-11 02:13:18 -0500

    added th'aboot page, eh?

--- about.php
+++ about.php
@@ -1,7 +1,36 @@
 <?php $tab="about"; require "includes/header.php" ?>
+<div id="intro">
+	<div id="statement">
+		<h1>Our Mission</h1>
+		<p>
+			Combining low-tech hardware with sophisticated open-source software,
+			we explore novel ways to both empower young people and improve UNICEF's
+			operations.  We seek to connect youth who have access to information on
+			the Internet to those who do not, through innovations in technology and
+			program initiatives that are educational and inclusive. We also find ways
+			to integrate appropriate technological systems into existing UNICEF working
+			practices to augment and improve the effectiveness of our operations
+		</p>
+		<p>
+			We collaborate with others to make this happen, convening development
+			professionals, academia, and the private sector.
+		</p>
+	</div>
+</div>
 <div id="content">
 	<div class="nest">
-		Nothing to see here
+		<h2>History</h2>
+		<p>In early 2007, Dr. Sharad Sapra, UNICEF’s Director of the Division of Communication posed a series of questions to UNICEF staff about how the rapid technological change the world is experiencing can benefit UNICEF's mission and work.</p>
+		<ul>
+			<li>&quot;How to connect youth who are connected to those who are not?&quot;</li>
+			<li>&quot;How can the explosion of mobile phone users and networks change they way development work is done?&quot;</li>
+			<li>&quot;How can UNICEF use the power of ever growing social networks of youth to educate, inform and empower?&quot;</li>
+		</ul>
+		<p>A group of interested people, starting in UNICEF’s Division of Communication, but expanding to other areas within and outside UNICEF started thinking about what this would mean for children around the world and began exploring different ways to address this challenge.</p>
+		<h2>UNICEF</h2>
+		<p>The UNICEF Innovation Team sits in the Youth Section of the Department of Communications in the New York Headquarters of UNICEF.</p>
+		<h2>Open Source and Creative Commons</h2>
+		<p>All of the computer code we write is free and open source.  We want to encourage anyone around the world customize our projects to fit their needs, to localize our products for their region, or just use the software to solve problems they have. The majority of the code is licensed under the AGPL or GPL.  Much of the content created on our sites is licensed under Creative Commons.  Providing the code for free allows it serve the widest audience, even a dollar liscening fee can be prohibitive.</p>
 	</div>
 </div>
 <?php require "includes/footer.php" ?>
