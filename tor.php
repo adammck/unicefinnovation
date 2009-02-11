06e56273b0e33103c2092ea7a356946bd4ecd6e3
Merrick Schaefer <merrickweb@floating-point.(none)>
2009-02-11 05:49:28 -0500

    added terms of use page

---
+++ tor.php
@@ -0,0 +1,33 @@
+<?php require "includes/header.php" ?>
+<div id="content">
+	<div class="nest">
+		<h2 class="first">Terms of Use</h2>
+		<p>Use of this Web site, www.unicefinnovation.org and all pages in this domain (together, the "UNICEF Innovation Web Site"), constitutes acceptance of the following terms and conditions. UNICEF reserves the right to seek all remedies available by law for any violation of these terms of use, including any violation of UNICEF's rights in the UNICEF Innovation or UNICEF name and emblem and its rights in connection with the information, artwork, text, video, audio, or pictures (together, the "Content") of the UNICEF Innovation Web Site.</p>
+
+		<h3>The UNICEF Name and Emblem</h3>
+		<p>The UNICEF name and emblem are the exclusive property of UNICEF. They are protected under international law. Unauthorized use is prohibited. They may not be copied or reproduced in any way without the prior written permission of UNICEF. Requests for permission should be sent to us using the copyright permission option our feedback form.</p>
+
+		<h3>The UNICEF Innovation Web Site</h3>
+		<p>The UNICEF Innovation Web Site is provided by UNICEF for personal use and educational purposes only. All Content of the UNICEF Innovation Web Site is protected by law according to the Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 United States License.</p>
+		<p>The Content of the UNICEF Innovation Web Site is selected on the basis of UNICEF's own criteria and in its own sole discretion. UNICEF reserves the right at any time to change or discontinue any aspect or feature of the UNICEF Innovation Web Site, including but not limited to these terms of use, hours of availability and equipment needed for use.</p>
+		<p>The use of particular designations of countries or territories does not imply any judgment by UNICEF as to the legal status of such countries or territories, of their authorities and institutions or of the delimitation of their boundaries. The mention of names of specific companies or products (whether or not indicated as registered) does not imply any intention to infringe proprietary rights, nor should it be construed as an endorsement or recommendation on the part of UNICEF.</p>
+		<p>The United Nations and UNICEF disclaim any liability or responsibility arising from the use of the UNICEF Innovation Web Site or the Content of the UNICEF Innovation Web Site. The United Nations, UNICEF, members of their staff, and their contractors, shall not be liable for any financial or other consequences whatsoever arising from the use of the Content of the UNICEF Innovation Web Site, including the inappropriate, improper, or fraudulent use of such Content.</p>
+		<p>No representations or warranties of any kind concerning the UNICEF Innovation Web Site are given, including responsibility for any infection by virus or any other contamination or by anything which has destructive properties. </p>
+
+		<h3>Other Web Sites</h3>
+		<p>The UNICEF Innovation Web Site may be linked to other web sites that are not under UNICEF's control. UNICEF provides these links merely as a convenience and the inclusion of such links does not imply an endorsement or approval by UNICEF of any web site, product or service. UNICEF does not assume any responsibility or liability in respect of such web sites, including, for example, responsibility or liability for the accuracy or reliability of any information, data, opinions, advice or statements made on those web sites.</p>
+
+		<h3>Working Papers</h3>
+		<p>The UNICEF Web Site may contain Working Papers code or discussion drafts of UNICEF Policy Papers. Comments and responses are welcome, but these should in all cases be addressed directly to UNICEF and not uploaded to the UNICEF Innovation Web Site. These documents are provided by UNICEF as a contribution to discussion on important issues affecting children and families, and to assist UNICEF by broadening and deepening the range of the consultations engaged in by the Organization. Such documents do not represent the Organization's definitive position on the issues covered and are not to be represented as such. Working papers are subject to the license that they are issued under.</p>
+
+		<h3>UNICEF Logo</h3>
+		<p>The unauthorized use of the UNICEF name and logo is against international law and is expressly forbidden.</p>
+
+		<!--<h3>Interactive Parts of the UNICEF Innovation Web Site</h3>
+		<p>The UNICEF Innovation Web Site may contain bulletin boards, chat rooms, access to mailing lists or other message or communication facilities. The user agrees, when taking advantage of such facilities, not to do any of the following: (a) defame, abuse, harass, stalk, threaten or otherwise violate the legal, moral, or human rights of others (such as rights of privacy and publicity); (b) publish, post, distribute or disseminate any defamatory, infringing, obscene, indecent or unlawful material or information; (c) upload or attach files that contain software or other material protected by intellectual property laws (or by rights of privacy) unless the user owns or controls the rights thereto or has received all necessary consents; (d) upload or attach files that contain viruses, corrupted files, or any other similar software or programmes that may damage the operation of another's computer; (e) delete any author attributions, legal notices or proprietary designations or labels in any file that is uploaded; (f) falsify the origin or source of software or other material contained in a file that is uploaded; (g) advertise or offer to sell any goods or services, or conduct or forward surveys, contests, or chain letters; or (h) download any file posted by another user that the user knows, or reasonably should know, cannot be legally distributed in such manner. UNICEF reserves the absolute right to delete any material posted to the UNICEF Innovation Web Site without notice to the user.</p>
+		<p>UNICEF accepts no responsibility or liability in respect of the conduct of any user in connection with such facilities, or for any material submitted by users and carried on the UNICEF Web Site including, for example, responsibility or liability for the accuracy or reliability of any information, data, opinions, advice or statements made in such material</p>-->
+
+		<p>If you have any questions about this please contact UNICEF. <br />Thank you.</p>
+	</div>
+</div>
+<?php require "includes/footer.php" ?>
